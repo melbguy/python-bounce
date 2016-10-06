70309ec2a6089f3e02bd611c76517d90c8e52a79
pythonclass <mallawa.perera@gmail.com>
2016-10-05 19:37:55 -0700

    My First commit

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import sys, pygame
 
 pygame.init()
-size = width, height = 320, 240
-speed = [2, 2]
+size = width, height = 520, 600
+speed = [4, 4]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
